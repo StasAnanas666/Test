3105951dfda6a27a2910b3a57254f42619c683ab
StasAnanas666 <124436134+StasAnanas666@users.noreply.github.com>
2023-03-19 15:52:23 +0700

    Update StandartInterfaces.cs

--- StandartInterfaces.cs
+++ StandartInterfaces.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,7 +22,63 @@ namespace InterfacesWPU221
         public string firstName { get; set; }
         public string lastName { get; set; }
         public DateTime birthDate { get; set; }
+        public string group { get; set; }
         public StudentCard StudentCard { get; set; }
+    }
+
+    class Auditory
+    {
+        Student[] students =
+        {
+            new Student
+            {
+                firstName="Harry",
+                lastName="Potter",
+                birthDate=new DateTime(1981,07,31),
+                group="Griffindor",
+                StudentCard=new StudentCard
+                {
+                    Number=19810731,
+                    Series="HP"
+                }
+            },
+            new Student
+            {
+                firstName="Drako",
+                lastName="Malfoy",
+                birthDate=new DateTime(1981,10,23),
+                group="Slitherin",
+                StudentCard=new StudentCard
+                {
+                    Number=19811023,
+                    Series="DM"
+                }
+            },
+            new Student
+            {
+                firstName="Polumna",
+                lastName="Lovegood",
+                birthDate=new DateTime(1985,03,15),
+                group="Hufflepuff",
+                StudentCard=new StudentCard
+                {
+                    Number=19850315,
+                    Series="PL"
+                }
+            },
+            new Student
+            {
+                firstName="Cedrick",
+                lastName="Diggory",
+                birthDate=new DateTime(1980,09,25),
+                group="Ravenclaw",
+                StudentCard=new StudentCard
+                {
+                    Number=19800925,
+                    Series="CD"
+                }
+            }
+        };
     }
 
     class InterfacesWPU221
