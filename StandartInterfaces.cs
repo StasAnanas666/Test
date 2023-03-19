2d0b5163c54d1252d9efdca2645b089532f4177a
StasAnanas666 <124436134+StasAnanas666@users.noreply.github.com>
2023-03-19 15:42:26 +0700

    Add files via upload

---
+++ StandartInterfaces.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace InterfacesWPU221
+{
+    class StudentCard
+    {
+        public int Number { get; set; }
+        public string Series { get; set; }
+        public override string ToString()
+        {
+            return $"Студенческий билет: {Series} {Number}\n";
+        }
+    }
+
+    class Student
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public DateTime birthDate { get; set; }
+        public StudentCard StudentCard { get; set; }
+    }
+
+    class InterfacesWPU221
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
