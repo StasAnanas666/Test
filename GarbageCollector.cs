7cecfc5ad6f81f55a13d99a1e5d81049d605bf11
StasAnanas666 <124436134+StasAnanas666@users.noreply.github.com>
2023-03-19 18:05:57 +0700

    Add files via upload

---
+++ GarbageCollector.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.GC;
+using static System.Exception;
+using static System.Console;
+
+//все объекты создаются в динамической управляемой памяти - куче, которая для удобства и эффективности работы сборщика мусора делится на 3 поколения: 0, 1 и 2.
+//0 поколение занимает 256Кбайт
+//1 поколение занимает 2Мбайт
+//2 поколение занимает 10 Мбайт
+
+namespace InterfacesWPU221
+{
+    
+
+    class GarbageCollector
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Демонстрация работы класса System.GC");
+            WriteLine($"Максимальное поколение: {MaxGeneration}");
+        }
+    }
+}
